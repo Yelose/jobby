be9b2f80b1186053f9f4e8e6c87e52c7e16653d3
Marina González <yelose85@gmail.com>
2023-04-10 08:19:42 +0200

    firebase setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Firestore, getDocs, collection } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'jobby';
+export class AppComponent implements OnInit {
+  constructor(private firestore: Firestore) {}
+
+  querySnapshot: any;
+
+  async getData() {
+    console.log('Getting data!');
+    this.querySnapshot = await getDocs(collection(this.firestore, 'test'));
+    this.querySnapshot.forEach((document: any) => {
+      console.log(`${document.id} => ${document.data().name}`);
+    });
+  }
+  ngOnInit(): void {
+    this.getData();
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'yelose-jobby',
+    appId: '1:983960327341:web:85096b5eee1f9db5276224',
+    storageBucket: 'yelose-jobby.appspot.com',
+    apiKey: 'AIzaSyBW4-alQekO8G0CaOSOB7oG1-niCvUrxng',
+    authDomain: 'yelose-jobby.firebaseapp.com',
+    messagingSenderId: '983960327341',
+  },
+  production: false,
+};
